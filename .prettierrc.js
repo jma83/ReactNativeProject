4d87d0f9a530750bd70063048cd592bceca120dc
javier.martinez <jma96.elx@gmail.com>
2021-07-22 22:08:42 +0200

    First commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  trailingComma: 'none',
+  tabWidth: 2,
+  endOfLine: 'auto',
+};
